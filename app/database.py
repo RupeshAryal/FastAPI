2ff8da82f09957f12e2169267943c817b49b25d0
RupeshAryal <aryalrupesh2074@gmail.com>
2023-10-03 19:03:13 +0545

    database connection

--- app/database.py
+++ app/database.py
@@ -1,7 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-import models
 
 SQLALCHEMY_DATABASE_URL = 'mysql+mysqlconnector://root:root@localhost/sqlalchemy'
 engine = create_engine(SQLALCHEMY_DATABASE_URL)

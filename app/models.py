ac6aa8870165c426d564e8ce32c6f8e29a58e712
RupeshAryal <aryalrupesh2074@gmail.com>
2023-10-04 18:14:22 +0545

    sql alchemy connection

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from app.database import Base
-from sqlalchemy import Column, String, Integer, Boolean
+from sqlalchemy import Column, String, Integer, Boolean, TIMESTAMP, text
+
 
 class Post(Base):
     __tablename__ = "posts"
@@ -7,6 +8,7 @@ class Post(Base):
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     title = Column(String(200), nullable=False)
     content = Column(String(1000), nullable=False)
-    published = Column(Boolean, default=True)
+    published = Column(Boolean, server_default=text('TRUE'), nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
 
 

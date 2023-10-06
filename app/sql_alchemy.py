bf71e14432c4834089be326a7673447ac51630ba
RupeshAryal <aryalrupesh2074@gmail.com>
2023-10-06 19:18:10 +0545

    moved all routes to user and posts to routers

--- app/sql_alchemy.py
+++ app/sql_alchemy.py
@@ -1,79 +1,12 @@
-from fastapi import FastAPI, Response, status, HTTPException, Depends
-from pydantic import BaseModel, ConfigDict
-
-from sqlalchemy import select
-from sqlalchemy.orm import Session
-from app.database import engine, get_db
+from fastapi import FastAPI
+from app.database import engine
 from app import models
+from .routers import post, user
+
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-
-class Post(BaseModel):
-    model_config = ConfigDict(extra='forbid')
-    title: str
-    content: str
-    published: bool = True
-
-
-@app.get("/posts")
-def get_posts(db: Session = Depends(get_db)):
-    posts = db.query(models.Post).all()
-    print(posts)
-    return {'data': posts}
-
-
-@app.post("/posts", status_code=status.HTTP_201_CREATED)
-def add_post(post: Post, db: Session = Depends(get_db)):
-    new_post = models.Post(
-        **post.model_dump())
-    db.add(new_post)
-    db.commit()
-    db.refresh(new_post)
-
-    return {'detail': 'post added successfully'}
-
-
-@app.get("/posts/{id}")
-def get_post_id(id: int, db: Session = Depends(get_db)):
-    query_result = db.query(models.Post).filter(models.Post.id == id)
-
-    post = query_result.first()
-    if post == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"post with id: {id} does not exist"
-                            )
-    return {'posts': post}
-
-@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int, db: Session = Depends(get_db)):
-    post = db.query(models.Post).filter(models.Post.id == id)
-
-    if post.first() == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"post with id : {id} does not exist")
-
-    post.delete(synchronize_session = False)
-    db.commit()
-
-    return {"detail": "post deleted"}
-
-@app.put("/posts/{id}")
-def update_post(id: int, post: Post, db: Session = Depends(get_db)):
-
-    post_query = db.query(models.Post).filter(models.Post.id == id)
-    post1 = post_query.first()
-    if post1 is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail="post not found")
-
-    post_query.update(post.model_dump(), synchronize_session=False)
-    db.commit()
-
-    return {"data": "successful"}
-
-
-
+app.include_router(post.router)
+app.include_router(user.router)
 

ccecb8ef3d07636b8bd18dc532c41d18b1e7f1cd
RupeshAryal <aryalrupesh2074@gmail.com>
2023-10-12 21:19:54 +0545

    fixed typo

--- app/routers/user.py
+++ app/routers/user.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
 from sqlalchemy.orm import Session
 from ..database import get_db
 
-from .. import models, schemas, utils
+from .. import models, schemas, utils, oauth2
 
 router = APIRouter(
     prefix="/users",

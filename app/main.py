d55a35da9887c2fde37fac304973219e84cc57fb
RupeshAryal <aryalrupesh2074@gmail.com>
2023-10-19 21:43:43 +0545

    voting system added

--- app/main.py
+++ app/main.py
@@ -7,7 +7,11 @@ from .routers import post, user, auth
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
 app.include_router(post.router)
 app.include_router(user.router)
-
 app.include_router(auth.router)
+
+@app.get("/")
+def home():
+    return {"message": "Hello world!!"}

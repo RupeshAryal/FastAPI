a49280e2d6c66b1e77c578f7aa6749a4af7a571a
RupeshAryal <aryalrupesh2074@gmail.com>
2023-10-19 21:46:03 +0545

    removed hardcoded url and applied environment variables

--- app/database.py
+++ app/database.py
@@ -1,7 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from .config import settings
 
-SQLALCHEMY_DATABASE_URL = 'mysql+mysqlconnector://root:root@localhost/sqlalchemy'
+SQLALCHEMY_DATABASE_URL = (f"{settings.database_username}://{settings.database_hostname}:{settings.database_password}@"
+                           f"{settings.database_port}/{settings.database_name}")
+
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 

459a98742d97e953fc71c4713f0dfa0b60bed204
RupeshAryal <aryalrupesh2074@gmail.com>
2023-10-20 22:34:31 +0545

    joins applied

--- app/routers/post.py
+++ app/routers/post.py
@@ -1,5 +1,6 @@
 from fastapi import status, HTTPException, Depends, APIRouter, Depends
 from sqlalchemy.orm import Session
+from sqlalchemy import func, select
 from ..database import get_db
 
 from .. import models, schemas, oauth2
@@ -15,11 +16,13 @@ router = APIRouter(
 def get_posts(db: Session = Depends(get_db),
               current_user: int = Depends(oauth2.get_current_user)):
 
-    posts = db.query(models.Post).filter(models.Post.owner_id == current_user.id).all()
+    posts = db.query(models.Post).all()
 
-    results = db.query(models.Post).join(
-        models.Vote, models.Vote.post_id == models.Post.id, isouter=True).groupby(models.Post.id)
+    # results = db.query(models.Post, func.count(models.Vote.post_id).label("count")). \
+    #     join(models.Vote, models.Vote.post_id == models.Post.id, isouter=True). \
+    #     group_by(models.Post.id).all()
 
+    # print(results)
 
     return posts
 
@@ -45,7 +48,13 @@ def get_post_id(id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id: {id} does not exist"
                             )
-    return post
+
+    results = db.query(models.Post.id, func.count(models.Vote.post_id).label("count")).\
+        join(models.Vote, models.Vote.post_id == models.Post.id).\
+        filter(models.Post.id == id).\
+        group_by(models.Post.id).first()
+
+    return post, {"likes": results.count}
 
 
 @router.delete("/{post_id}", status_code=status.HTTP_204_NO_CONTENT)
